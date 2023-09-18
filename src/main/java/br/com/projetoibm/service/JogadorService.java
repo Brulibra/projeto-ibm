3b361cf94dc013a3c0f4a879cc4375f82c3557d8
Bruna de Paula <brulibra03@hotmail.com>
2023-09-18 18:26:11 -0300

    🚧 Deveria separar os times

--- src/main/java/br/com/projetoibm/service/JogadorService.java
+++ src/main/java/br/com/projetoibm/service/JogadorService.java
@@ -15,13 +15,51 @@ public class JogadorService {
 JogadorRepository jogadorRepository;
 
   public List<JogadorEntity> listaTimes() {
+
       return jogadorRepository.findAll();
   }
-  public void addNovoJogador(JogadorEntity jogador) {
+  public List<String> addNovoJogador(JogadorEntity jogador) {
+
+    List<String> umJogador = List.of(jogador.getJogadorTime());
+
       jogadorRepository.save(jogador);
+      return addNovoJogador(umJogador);
+  }
+  public List<String> addNovoJogador(List<String> jogador) {
+
+      Map<String, List<String>> times = new HashMap<>();
+
+      for(String novoTime : jogador){
+
+        List<String> nomeJogador = List.of(novoTime.split(" "));
+        String sobrenove = String.valueOf(nomeJogador.get(1));
+        String nomeDoTime = String.valueOf(sobrenove.charAt(0));
+
+        List<String> addNovoTime = times.get(nomeDoTime);
+
+        if (!times.containsKey(nomeDoTime)){
+          times.put(nomeDoTime, new ArrayList<>());
+          times.get(nomeDoTime).add(nomeJogador.toString());
+        } else {
+
+          for (String mesmoSobreNome : addNovoTime){
+            if(mesmoSobreNome.endsWith(sobrenove)){
+
+              times.put(sobrenove, new ArrayList<>());
+              times.get(sobrenove).add(nomeJogador.toString());
+            }
+          }
+          times.get(nomeDoTime).add(nomeJogador.toString());
+
+        }
+    }
+
+    return null;
   }
 
+
   public void deletaTimes(){
+
     jogadorRepository.deleteAll();
   }
 
